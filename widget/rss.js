4c8657adabda40d85afb70d47375553c27fb8897
Yann Vavasseur <32073576+yvavasseur@users.noreply.github.com>
2018-05-03 20:04:40 +0200

    Update rss.js

--- widget/rss.js
+++ widget/rss.js
@@ -1,67 +1,67 @@
-function myGetElementsByClassName(selector) {
-    if ( document.getElementsByClassName ) {
-        return document.getElementsByClassName(selector);
-    }
-    var returnList = new Array();
-    var nodes = document.getElementsByTagName('div');
-    var max = nodes.length;
-    for ( var i = 0; i < max; i++ ) {
-        if ( nodes[i].className == selector ) {
-            returnList[returnList.length] = nodes[i];
-        }
-    }
-    return returnList;
-}
-var rssReader = {
-    containers : null,
-    // initialization function
-    init : function(selector) {
-        containers = myGetElementsByClassName(selector);
-        for(i=0;i<containers.length;i++){
-            // getting necessary variables
-            var rssUrl = containers[i].getAttribute('rss_url');
-            var num = containers[i].getAttribute('rss_num');
-            var id = containers[i].getAttribute('id');
-            // creating temp scripts which will help us to transform XML (RSS) to JSON
-            var url = encodeURIComponent(rssUrl);
-            var googUrl = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num='+num+'&q='+url+'&callback=rssReader.parse&context='+id;
-            var script = document.createElement('script');
-            script.setAttribute('type','text/javascript');
-            script.setAttribute('charset','utf-8');
-            script.setAttribute('src',googUrl);
-            containers[i].appendChild(script);
-        }
-    },
-    // parsing of results by google
-    parse : function(context, data) {
-        var container = document.getElementById(context);
-        container.innerHTML = '';
-        // creating list of elements
-        var mainList = document.createElement('ul');
-        // also creating its childs (subitems)
-        var entries = data.feed.entries;
-        for (var i=0; i<entries.length; i++) {
-            var listItem = document.createElement('li');
-            var title = entries[i].title;
-            var contentSnippet = entries[i].contentSnippet;
-            var contentSnippetText = document.createTextNode(contentSnippet);
-            var link = document.createElement('a');
-            link.setAttribute('href', entries[i].link);
-            link.setAttribute('target','_blank');
-            var text = document.createTextNode(title);
-            link.appendChild(text);
-            // add link to list item
-            listItem.appendChild(link);
-            var desc = document.createElement('p');
-            desc.appendChild(contentSnippetText);
-            // add description to list item
-            listItem.appendChild(desc);
-            // adding list item to main list
-            mainList.appendChild(listItem);
-        }
-        container.appendChild(mainList);
-    }
-};
-window.onload = function() {
-    rssReader.init('post_results');
-}
+/*
+* FeedEk jQuery RSS/ATOM Feed Plugin v3.0 with YQL API
+* http://jquery-plugins.net/FeedEk/FeedEk.html  https://github.com/enginkizil/FeedEk
+* Author : Engin KIZIL http://www.enginkizil.com   
+*/
+
+(function ($) {
+    $.fn.FeedEk = function (opt) {
+        var def = $.extend({
+            MaxCount: 5,
+            ShowDesc: true,
+            ShowPubDate: true,
+            DescCharacterLimit: 0,
+            TitleLinkTarget: "_blank",
+            DateFormat: "",
+            DateFormatLang:"en"
+        }, opt);
+        
+        var id = $(this).attr("id"), i, s = "", dt;
+        $("#" + id).empty();
+        if (def.FeedUrl == undefined) return;       
+        $("#" + id).append('<img src="loader.gif" />');
+
+        var YQLstr = 'SELECT channel.item FROM feednormalizer WHERE output="rss_2.0" AND url ="' + def.FeedUrl + '" LIMIT ' + def.MaxCount;
+
+        $.ajax({
+            url: "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(YQLstr) + "&format=json&diagnostics=false&callback=?",
+            dataType: "json",
+            success: function (data) {
+                $("#" + id).empty();
+                if (!(data.query.results.rss instanceof Array)) {
+                    data.query.results.rss = [data.query.results.rss];
+                }
+                $.each(data.query.results.rss, function (e, itm) {
+                    s += '<li><div class="itemTitle"><a href="' + itm.channel.item.link + '" target="' + def.TitleLinkTarget + '" >' + itm.channel.item.title + '</a></div>';
+                    
+                    if (def.ShowPubDate){
+                        dt = new Date(itm.channel.item.pubDate);
+                        s += '<div class="itemDate">';
+                        if ($.trim(def.DateFormat).length > 0) {
+                            try {
+                                moment.lang(def.DateFormatLang);
+                                s += moment(dt).format(def.DateFormat);
+                            }
+                            catch (e){s += dt.toLocaleDateString();}                            
+                        }
+                        else {
+                            s += dt.toLocaleDateString();
+                        }
+                        s += '</div>';
+                    }
+                    if (def.ShowDesc) {
+                        s += '<div class="itemContent">';
+                         if (def.DescCharacterLimit > 0 && itm.channel.item.description.length > def.DescCharacterLimit) {
+                            s += itm.channel.item.description.substring(0, def.DescCharacterLimit) + '...';
+                        }
+                        else {
+                            s += itm.channel.item.description;
+                         }
+                         s += '</div>';
+                    }
+                });
+                $("#" + id).append('<ul class="feedEkList">' + s + '</ul>');
+            }
+        });
+    };
+})(jQuery);
