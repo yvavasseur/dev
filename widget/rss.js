afa721ac3bf9e8328e2a7f2b8d78a773b89c688b
Yann Vavasseur <32073576+yvavasseur@users.noreply.github.com>
2018-05-03 20:25:41 +0200

    Update rss.js

--- widget/rss.js
+++ widget/rss.js
@@ -1,67 +1,53 @@
-/*
-* FeedEk jQuery RSS/ATOM Feed Plugin v3.0 with YQL API
-* http://jquery-plugins.net/FeedEk/FeedEk.html  https://github.com/enginkizil/FeedEk
-* Author : Engin KIZIL http://www.enginkizil.com   
-*/
+      bodyPlain: (function (entry) {
+        var result = entry.content
+          .replace(/<script[\\r\\\s\S]*<\/script>/mgi, '')
+          .replace(/<\/?[^>]+>/gi, '');
 
-(function ($) {
-    $.fn.FeedEk = function (opt) {
-        var def = $.extend({
-            MaxCount: 5,
-            ShowDesc: true,
-            ShowPubDate: true,
-            DescCharacterLimit: 0,
-            TitleLinkTarget: "_blank",
-            DateFormat: "",
-            DateFormatLang:"en"
-        }, opt);
-        
-        var id = $(this).attr("id"), i, s = "", dt;
-        $("#" + id).empty();
-        if (def.FeedUrl == undefined) return;       
-        $("#" + id).append('<img src="loader.gif" />');
+        for (var i = 0; i < RSS.htmlTags.length; i++) {
+          result = result.replace(new RegExp('<' + RSS.htmlTags[i], 'gi'), '');
+        }
 
-        var YQLstr = 'SELECT channel.item FROM feednormalizer WHERE output="rss_2.0" AND url ="' + def.FeedUrl + '" LIMIT ' + def.MaxCount;
+        return result;
+      })(entry),
 
-        $.ajax({
-            url: "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(YQLstr) + "&format=json&diagnostics=false&callback=?",
-            dataType: "json",
-            success: function (data) {
-                $("#" + id).empty();
-                if (!(data.query.results.rss instanceof Array)) {
-                    data.query.results.rss = [data.query.results.rss];
-                }
-                $.each(data.query.results.rss, function (e, itm) {
-                    s += '<li><div class="itemTitle"><a href="' + itm.channel.item.link + '" target="' + def.TitleLinkTarget + '" >' + itm.channel.item.title + '</a></div>';
-                    
-                    if (def.ShowPubDate){
-                        dt = new Date(itm.channel.item.pubDate);
-                        s += '<div class="itemDate">';
-                        if ($.trim(def.DateFormat).length > 0) {
-                            try {
-                                moment.lang(def.DateFormatLang);
-                                s += moment(dt).format(def.DateFormat);
-                            }
-                            catch (e){s += dt.toLocaleDateString();}                            
-                        }
-                        else {
-                            s += dt.toLocaleDateString();
-                        }
-                        s += '</div>';
-                    }
-                    if (def.ShowDesc) {
-                        s += '<div class="itemContent">';
-                         if (def.DescCharacterLimit > 0 && itm.channel.item.description.length > def.DescCharacterLimit) {
-                            s += itm.channel.item.description.substring(0, def.DescCharacterLimit) + '...';
-                        }
-                        else {
-                            s += itm.channel.item.description;
-                         }
-                         s += '</div>';
-                    }
-                });
-                $("#" + id).append('<ul class="feedEkList">' + s + '</ul>');
-            }
-        });
-    };
+      shortBodyPlain: entry.contentSnippet.replace(/<\/?[^>]+>/gi, ''),
+      index:          $.inArray(entry, this.entries),
+      totalEntries:   this.entries.length,
+
+      teaserImage:    (function (entry) {
+        try {
+          return entry.content.match(/(<img.*?>)/gi)[0];
+        }
+        catch (e) {
+          return '';
+        }
+      })(entry),
+
+      teaserImageUrl: (function (entry) {
+        try {
+          return entry.content.match(/(<img.*?>)/gi)[0].match(/src="(.*?)"/)[1];
+        }
+        catch (e) {
+          return '';
+        }
+      })(entry)
+    }, this.options.tokens);
+  };
+
+  RSS.prototype.getValueForToken = function (_token, entry) {
+    var tokenMap = this.getTokenMap(entry);
+    var token    = _token.replace(/[\{\}]/g, '');
+    var result   = tokenMap[token];
+
+    if (typeof result !== 'undefined') {
+      return ((typeof result === 'function') ? result(entry, tokenMap) : result);
+    } else {
+      throw new Error('Unknown token: ' + _token + ', url:' + this.url);
+    }
+  };
+
+  $.fn.rss = function (url, options, callback) {
+    new RSS(this, url, options, callback).render();
+    return this; // Implement chaining
+  };
 })(jQuery);
